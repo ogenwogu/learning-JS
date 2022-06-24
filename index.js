a514680194811f3ee41177b3966c8fc586ec5370
Ogechi <ogechinwogu77@gmail.com>
2022-06-24 01:55:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let myName = 'Ogechi Nwogu'
+console.log(myName);
+
+let myHeight = '164cm'
+console.log(myHeight);
+
+let myCountry = 'Nigeria'
+console.log(myCountry);
+
+alert ('My name is ' + myName + ', I am ' + myHeight + ' from ' + myCountry + '.' );
+
+// alternatively
+
+alert ('Name: Ogechi Nwogu \nHeight: 164cm \nCountry: Nigeria.');
